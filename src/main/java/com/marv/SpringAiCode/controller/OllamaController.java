03f7e8bdf12737438d0a903fa19cde5860272d91
MarvsConcept <Marvade8@gmail.com>
2026-06-13 02:41:24 -0700

    Implemented PGVector

--- src/main/java/com/marv/SpringAiCode/controller/OllamaController.java
+++ src/main/java/com/marv/SpringAiCode/controller/OllamaController.java
@@ -1,66 +1,66 @@
-package com.marv.SpringAiCode.controller;
-
-import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.memory.ChatMemory;
-import org.springframework.ai.chat.memory.MessageWindowChatMemory;
-import org.springframework.ai.chat.model.ChatResponse;
-import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.chat.prompt.PromptTemplate;
-import org.springframework.ai.ollama.OllamaChatModel;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
-
-@RestController
-public class OllamaController {
-
-    private ChatClient chatClient;
-
-    public OllamaController(OllamaChatModel chatModel) {
-        this.chatClient = ChatClient.create(chatModel);
-    }
-
-    ChatMemory chatMemory = MessageWindowChatMemory.builder().build();
-
-//    @GetMapping("/api/{message}")
-    public ResponseEntity<String> getAnswer(@PathVariable String message) {
-
-        ChatResponse chatResponse = chatClient
-                .prompt(message)
-                .call()
-                .chatResponse();
-
-        System.out.println(chatResponse.getMetadata().getModel());
-
-        String response = chatResponse
-                .getResult()
-                .getOutput()
-                .getText();
-
-        return ResponseEntity.ok(response);
-    }
-
-//    @PostMapping("/api/recommend")
-    public String recommend(@RequestParam String type,
-                            @RequestParam String year,
-                            @RequestParam String lang) {
-
-        String tempt = """
-                I want to watch a {type} movie tonight with a good rating,
-                looking for movies around this year {year}.
-                The language in looking for is {lang}.
-                Suggest one specific movie and tell me the cast and length of the movie
-                """;
-
-        PromptTemplate promptTemplate = new PromptTemplate(tempt);
-        Prompt prompt = promptTemplate.create(Map.of("type", type, "year", year, "lang", lang));
-
-        String response = chatClient
-                .prompt(prompt)
-                .call()
-                .content();
-
-        return response;
-    }
-}
+//package com.marv.SpringAiCode.controller;
+//
+//import org.springframework.ai.chat.client.ChatClient;
+//import org.springframework.ai.chat.memory.ChatMemory;
+//import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+//import org.springframework.ai.chat.model.ChatResponse;
+//import org.springframework.ai.chat.prompt.Prompt;
+//import org.springframework.ai.chat.prompt.PromptTemplate;
+//import org.springframework.ai.ollama.OllamaChatModel;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.Map;
+//
+//@RestController
+//public class OllamaController {
+//
+//    private ChatClient chatClient;
+//
+//    public OllamaController(OllamaChatModel chatModel) {
+//        this.chatClient = ChatClient.create(chatModel);
+//    }
+//
+//    ChatMemory chatMemory = MessageWindowChatMemory.builder().build();
+//
+////    @GetMapping("/api/{message}")
+//    public ResponseEntity<String> getAnswer(@PathVariable String message) {
+//
+//        ChatResponse chatResponse = chatClient
+//                .prompt(message)
+//                .call()
+//                .chatResponse();
+//
+//        System.out.println(chatResponse.getMetadata().getModel());
+//
+//        String response = chatResponse
+//                .getResult()
+//                .getOutput()
+//                .getText();
+//
+//        return ResponseEntity.ok(response);
+//    }
+//
+////    @PostMapping("/api/recommend")
+//    public String recommend(@RequestParam String type,
+//                            @RequestParam String year,
+//                            @RequestParam String lang) {
+//
+//        String tempt = """
+//                I want to watch a {type} movie tonight with a good rating,
+//                looking for movies around this year {year}.
+//                The language in looking for is {lang}.
+//                Suggest one specific movie and tell me the cast and length of the movie
+//                """;
+//
+//        PromptTemplate promptTemplate = new PromptTemplate(tempt);
+//        Prompt prompt = promptTemplate.create(Map.of("type", type, "year", year, "lang", lang));
+//
+//        String response = chatClient
+//                .prompt(prompt)
+//                .call()
+//                .content();
+//
+//        return response;
+//    }
+//}
